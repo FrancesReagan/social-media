16b7ce9fb9e74ebe1404162bf460c61b7a00af07
Frances Reagan James <adtgwen@gmail.com>
2025-07-14 15:04:31 -0400

    adding mongoose, User.js

---
+++ models/User.js
@@ -0,0 +1,60 @@
+
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+// define new schema//
+const userSchema = new Schema({
+  username: {
+    type: String,
+    // Field must be provided//
+    required: [true, 'Username is required'], 
+     // Must be unique in the collection//
+    unique: true,   
+    // Removes whitespace//                       
+    trim: true,      
+      // Minimum length//                       
+    minlength: 3,                         
+  },
+
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+     // Converts email to lowercase//
+    lowercase: true,              
+    // Regex for email format//        
+    match: [/.+\@.+\..+/, 'Please enter a valid email address'] 
+  },
+
+  age: {
+    type: Number,
+    // Minimum value//
+    min: [18, 'Must be at least 18 years old'], 
+     // Maximum value//
+    max: 120                                 
+  },
+
+  isVerfied: Boolean,
+
+  role: {
+    type: String,
+    enum: {
+      // The value must be one of these strings--make sure default value user is listed here as well//
+      values: ['user', 'admin'],            
+      message: '{VALUE} is not a supported role'
+    },
+     // If not provided, defaults to 'user'//
+    default: 'user'                        
+  },
+
+  // like a timestamp for my documents//
+  createdAt: {
+    type: Date,
+    // Sets the value to the current date/time//
+    default: () => Date.now(),    
+    // if set to true--it cannot be changed after creation//          
+    immutable: true                         
+  }
+
+});
--- server.js
+++ server.js
@@ -1,34 +1,21 @@
 import express from "express";
-import { MongoClient } from "mongodb";
+// import { MongoClient } from "mongodb";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 const PORT = 3000;
-
 const uri = process.env.MONGODB_URL;
-const client = new MongoClient(uri);
 
-// this will be moved to another file shortly from the async to the run function//
-async function run() {
-  try {
-  
-    await client.connect();
-    
-    await client.db("social-media").command({ ping: 1 });
-    console.log("Connected successfully to MongoDB!");
-  } finally {
-    
-    await client.close();
-  }
-}
-
-run().catch(console.dir);
-
+// connect to MongoDb using Mongoose//
+mongoose.connect(uri)
+.then(() => console.log("Connected successfully to MongoDb"))
+.catch(e => console.log(`Error connecting to MongoDb: ${e}`));
 
 app.get("/", (req, res) => {
   res.send("Hello, from API!");
-});
+})
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
